bc73c0c452d32069a7daa12c2027f5cdc405aa42
Sebastien Beuken <sb@shinka.be>
2017-11-17 17:31:19 +0100

    Add option --add-extra-oid to mode ap-list

--- centreon/common/airespace/snmp/mode/aplist.pm
+++ centreon/common/airespace/snmp/mode/aplist.pm
@@ -21,8 +21,96 @@ package centreon::common::airespace::snmp::mode::aplist;
   
 use base qw(centreon::plugins::mode);
 
+use Data::Dumper;
+
 use strict;
 use warnings;
+
+my %ap_type = (
+      1  => 'ap1000',
+      2  => 'ap1030',
+      3  => 'mimo',
+      4  => 'unknown',
+      5  => 'ap1100',
+      6  => 'ap1130',
+      7  => 'ap1240',
+      8  => 'ap1200',
+      9  => 'ap1310',
+      10 => 'ap1500',
+      11 => 'ap1250',
+      12 => 'ap1505',
+      13 => 'ap3201',
+      14 => 'ap1520',
+      15 => 'ap800',
+      16 => 'ap1140',
+      17 => 'ap800agn',
+      18 => 'ap3500i',
+      19 => 'ap3500e',
+      20 => 'ap1260',
+      21 => 'ap1040',
+      22 => 'ap1550',
+      23 => 'ap602i',
+      24 => 'ap3500p',
+      25 => 'ap802gn',
+      26 => 'ap802agn',
+      27 => 'ap3600i',
+      28 => 'ap3600e',
+      29 => 'ap2600i',
+      30 => 'ap2600e',
+      31 => 'ap802hagn',
+      32 => 'ap1600i',
+      33 => 'ap1600e',
+      34 => 'ap702e',
+      35 => 'ap702i',
+      36 => 'ap3600p',
+      37 => 'ap1530i',
+      38 => 'ap1530e',
+      39 => 'ap3700e',
+      40 => 'ap3700i',
+      41 => 'ap3700p',
+      42 => 'ap2700e',
+      43 => 'ap2700i',
+      44 => 'ap702w',
+      45 => 'wap2600i',
+      46 => 'wap2600e',
+      47 => 'wap1600i',
+      48 => 'wap1600e',
+      49 => 'wap702i',
+      50 => 'wap702e',
+      51 => 'ap1700i',
+      52 => 'ap1700e',
+      53 => 'ap1570e',
+      54 => 'ap1570i',
+      55 => 'ap1852e',
+      56 => 'ap1852i',
+      57 => 'ap1832i',
+      58 => 'unreported',
+      59 => 'apmr24',
+      60 => 'ap3702',
+      61 => 'ap1802i',
+      62 => 'ap1810w',
+      63 => 'apoeap1810',
+      64 => 'ap3802e',
+      65 => 'ap3802i',
+      66 => 'ap3802p',
+      67 => 'ap3802q',
+      68 => 'ap2802e',
+      69 => 'ap2802i',
+      70 => 'ap2802q',
+      71 => 'ap1815w',
+      72 => 'apoeap1815',
+      73 => 'ap1815I',
+      74 => 'ap1562e',
+      75 => 'ap1562i',
+      76 => 'ap1562d',
+      77 => 'ap1562ps',
+      78 => 'ap1800I',
+      79 => 'ap1800S',
+      80 => 'ap1815M',
+      81 => 'ap1542D',
+      82 => 'ap1542I',
+      83 => 'ap1815TSN'
+);
 
 sub new {
     my ($class, %options) = @_;
@@ -32,6 +120,7 @@ sub new {
     $options{options}->add_options(arguments =>
                                 {
                                     "filter-name:s"        => { name => 'filter_name' },
+                                    "add-extra-oid:s@"     => { name => 'add_extra_oid' },
                                 });
     $self->{ap_id_selected} = [];
 
@@ -42,7 +131,17 @@ sub check_options {
     my ($self, %options) = @_;
     $self->SUPER::init(%options);
 
-    # TODO : check options
+    $self->{extra_oids} = {};
+    foreach (@{$self->{option_results}->{add_extra_oid}}) {
+        next if ($_ eq '');
+        my ($name, $oid, $matching) = split /,/;
+        $matching = '%{instance}$' if (!defined($matching));
+        if (!defined($oid) || $oid !~ /^(\.\d+){1,}$/ || $name eq '') {
+            $self->{output}->add_option_msg(short_msg => "Wrong syntax for add-extra-oid '" . $_ . "' option.");
+            $self->{output}->option_exit();
+        }
+        $self->{extra_oids}->{$name} = { oid => $oid, matching => $matching };
+    }
 }
 
 sub run {
@@ -51,6 +150,7 @@ sub run {
 
     my $oid_bsnAPName = ".2.3.6.1.4.1.14179.2.2.1.1.3";
     my $oid_bsnAPModel = ".1.3.6.1.4.1.14179.2.2.1.1.16";
+    my $oid_bsnAPType = ".1.3.6.1.4.1.14179.2.2.1.1.22";
     my $oid_bsnAPLocation = ".1.3.6.1.4.1.14179.2.2.1.1.4";
     my $oid_bsnAPOperationStatus = ".1.3.6.1.4.1.14179.2.2.1.1.6";
     my $oid_bsnAPAdminStatus = ".1.3.6.1.4.1.14179.2.2.1.1.37";
@@ -79,7 +179,13 @@ sub run {
             $statstr = $statstr . 'DOWN.';
         }
 
-        $self->{output}->output_add(long_msg => $ap_name.$statstr);
+        my $extra_values = $self->get_extra_values_by_instance(instance => $_);
+        my $extra_display = '';
+        foreach my $name (keys %{$extra_values}) {
+            $extra_display .= ' - ' . $name . ' : ' . $extra_values->{$name};
+        }
+
+        $self->{output}->output_add(long_msg => $ap_name.$statstr.$extra_display);
     }
 
     $self->{output}->output_add(severity => 'OK',
@@ -93,9 +199,16 @@ sub manage_selection {
 
     my $oid_bsnAPName = ".1.3.6.1.4.1.14179.2.2.1.1.3";
     my $oid_bsnAPOperationStatus = ".1.3.6.1.4.1.14179.2.2.1.1.6";
+    my $oid_bsnAPType = ".1.3.6.1.4.1.14179.2.2.1.1.22";
     my $oid_bsnAPAdminStatus = ".1.3.6.1.4.1.14179.2.2.1.1.37";
     my $oids = [{ oid => $oid_bsnAPName }]; # , { oid => $oid_bsnAPOperationStatus }, { oid => $oid_bsnAPAdminStatus }];
 
+    if (scalar(keys %{$self->{extra_oids}}) > 0) {
+        foreach (keys %{$self->{extra_oids}}) {
+            push @$oids, { oid => $self->{extra_oids}->{$_}->{oid} };
+        }
+    }
+    
     $self->{datas} = {};
     $self->{results} = $self->{snmp}->get_multiple_table(oids => $oids);
     $self->{datas}->{all_ids} = [];
@@ -134,6 +247,7 @@ sub get_additional_information {
 
     my $oid_bsnAPName = ".1.3.6.1.4.1.14179.2.2.1.1.3";
     my $oid_bsnAPModel = ".1.3.6.1.4.1.14179.2.2.1.1.16";
+    my $oid_bsnAPType = ".1.3.6.1.4.1.14179.2.2.1.1.22";
     my $oid_bsnAPLocation = ".1.3.6.1.4.1.14179.2.2.1.1.4";
     my $oid_bsnAPOperationStatus = ".1.3.6.1.4.1.14179.2.2.1.1.6";
     my $oid_bsnAPAdminStatus = ".1.3.6.1.4.1.14179.2.2.1.1.37";
@@ -141,6 +255,7 @@ sub get_additional_information {
     my $oids = [];
     push @$oids, $oid_bsnAPName;
     push @$oids, $oid_bsnAPModel;
+    push @$oids, $oid_bsnAPType;
     push @$oids, $oid_bsnAPLocation;
     push @$oids, $oid_bsnAPOperationStatus;
     push @$oids, $oid_bsnAPAdminStatus;
@@ -149,10 +264,34 @@ sub get_additional_information {
     return $self->{snmp}->get_leef();
 }
 
+sub get_extra_values_by_instance {
+    my ($self, %options) = @_;
+    
+    my $extra_values = {};
+    foreach my $name (keys %{$self->{extra_oids}}) {
+        my $matching = $self->{extra_oids}->{$name}->{matching};
+        $matching =~ s/%\{instance\}/$options{instance}/g;
+        next if (!defined($self->{results}->{ $self->{extra_oids}->{$name}->{oid} }));
+        
+        my $append = '';
+        foreach (keys %{$self->{results}->{ $self->{extra_oids}->{$name}->{oid} }}) {
+            if (/^$self->{extra_oids}->{$name}->{oid}\.$matching/) {
+                $extra_values->{$name} = '' if (!defined($extra_values->{$name}));
+                $extra_values->{$name} .= $append . $self->{results}->{$self->{extra_oids}->{$name}->{oid}}->{$_};
+                $append = ',';
+            }
+        }
+    }
+    return $extra_values;
+}
+
 sub disco_format {
     my ($self, %options) = @_;
 
-    my $names = ['name', 'operStatus', 'adminStatus'];
+    my $names = ['name', 'operStatus', 'adminStatus', 'type'];
+    if (scalar(keys %{$self->{extra_oids}}) > 0) {
+        push @$names, keys %{$self->{extra_oids}};
+    }
     $self->{output}->add_disco_format(elements => $names);
 }
 
@@ -162,6 +301,7 @@ sub disco_show {
 
     my $oid_bsnAPName = ".1.3.6.1.4.1.14179.2.2.1.1.3";
     my $oid_bsnAPModel = ".1.3.6.1.4.1.14179.2.2.1.1.16";
+    my $oid_bsnAPType = ".1.3.6.1.4.1.14179.2.2.1.1.22";
     my $oid_bsnAPLocation = ".1.3.6.1.4.1.14179.2.2.1.1.4";
     my $oid_bsnAPOperationStatus = ".1.3.6.1.4.1.14179.2.2.1.1.6";
     my $oid_bsnAPAdminStatus = ".1.3.6.1.4.1.14179.2.2.1.1.37";
@@ -174,13 +314,17 @@ sub disco_show {
         my $ap_name = $result->{$oid_bsnAPName . '.' . $_};
         my $admin_status = $result->{$oid_bsnAPAdminStatus . '.' . $_};
         my $oper_status = $result->{$oid_bsnAPOperationStatus . '.' . $_};
+        my $ap_type = $self->{ap_type}->{$result->{$oid_bsnAPType . '.' . $_};}
 
         my ($opstr, $admstr);
         if ($oper_status == 1) { $opstr = 'up'; } else { $opstr = 'down'; }
         if ($admin_status == 1) { $admstr = 'up'; } else { $admstr = 'down'; }
+
+        my $extra_values = $self->get_extra_values_by_instance(instance => $_);
         $self->{output}->add_disco_entry(name => $ap_name,
                                          operStatus => $opstr,
-                                         adminStatus => $admstr);
+                                         adminStatus => $admstr,
+                                         %$extra_values);
     }
 }
 
